972bbef07c808ee18babd281133ec98e3c506e50
Gary Jarrel <gary@jarrel.com.au>
2025-05-27 12:09:30 +1000

    fix: extend `ALLOWED_HOSTS` in local setting so that it launches on local host

--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -1,5 +1,8 @@
 from config.settings.base import *
 
+# add local hosts to ALLOWED_HOSTS, 0.0.0.0 will bind to all interfaces
+ALLOWED_HOSTS = [*ALLOWED_HOSTS, 'localhost', '0.0.0.0', '127.0.0.1', 'yads.local']  # noqa: S104
+
 # in local we use Whitenoise to server static files as we use uvicorn in development
 sec_middleware_index = MIDDLEWARE.index('django.middleware.security.SecurityMiddleware')
 MIDDLEWARE.insert(sec_middleware_index + 1, 'whitenoise.middleware.WhiteNoiseMiddleware')
