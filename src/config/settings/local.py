6ce50d2b7ba77d01a1b59656bfca012dcd46d254
Gary Jarrel <gary@jarrel.com.au>
2025-05-27 16:07:36 +1000

    fix: fix double storage settings in local.py and use manifest file storage for static

--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -7,18 +7,14 @@ ALLOWED_HOSTS = [*ALLOWED_HOSTS, 'localhost', '0.0.0.0', '127.0.0.1', 'yads.loca
 sec_middleware_index = MIDDLEWARE.index('django.middleware.security.SecurityMiddleware')
 MIDDLEWARE.insert(sec_middleware_index + 1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
-# STATIC
-STORAGES = {
-    'default': {'BACKEND': 'django.core.files.storage.FileSystemStorage'},
-    'staticfiles': {'BACKEND': 'pdj.utils.storages.LocalManifestStaticStorage'},
-}
 
-
+# STATIC Config closer mimic to production using whitenoise and ManifestStaticFilesStorage
+# this is tested via `compose-prod.yaml`
 STORAGES = {
     'default': {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
     },
 }
